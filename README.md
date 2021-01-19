a83a07e15d207d6110d0a8febdc702372b9bd968
yourmean <lym0112@uos.ac.kr>
2021-01-19 19:28:52 +0900

    web

---
+++ README.md
@@ -0,0 +1,19 @@
+# Moving-Emoji-Generation - Web team 📖
+preview : https://yourmean.github.io/Moving-Emoji-Generation_Web/
+<p align="center"><img src="https://user-images.githubusercontent.com/55529646/104988631-f098c680-5a5b-11eb-97ab-c349a954672c.jpg" width="850" height="485"></p>
+<br>
+
+
+## DEMO SCREENSHOT
+<p align="center"><img src="https://user-images.githubusercontent.com/55529646/104742200-80552100-578d-11eb-9b2e-ab6464d9b285.jpg" width="850" height="850"></p>
+
+## Contributor 🌟
+<table>
+  <tr>
+    <td align="center"><a href="https://github.com/simba-pumba"><img src="https://user-images.githubusercontent.com/41895063/104711155-576c6600-5764-11eb-8a1e-a7a011cfe17d.png" width="150" height="150"><br /><sub><b>YeJi Lee</b></sub></td>
+    <td align="center"><a href="https://github.com/yourmean"><img src="https://user-images.githubusercontent.com/41895063/104711276-7cf96f80-5764-11eb-8473-99c5c0dc8a8a.png" width="150" height="150"><br /><sub><b>YuMin Lee</b></sub></td>
+    <td align="center"><a href="https://github.com/minkyeong"><img src="https://user-images.githubusercontent.com/55529646/104719170-6a386800-576f-11eb-8b55-36b524c4d166.jpg" width="150" height="150"><br /><sub><b>MinKyeong Kim</b></sub></td>
+    <td align="center"><a href="https://github.com/Jeong-JaeYoon"><img src="https://user-images.githubusercontent.com/55529646/104719173-6b699500-576f-11eb-9a42-a8569be057bc.jpg" width="150" height="150"><br /><sub><b>JaeYoon Jeong</b></sub></td>
+    <td align="center"><a href="https://github.com/Yu-Jin22"><img src="https://user-images.githubusercontent.com/41895063/104711416-afa36800-5764-11eb-85c1-1a9ad50033b7.png" width="150" height="150"><br /><sub><b>YuJin Han</b></sub></td>
+  </tr>
+</table>
---
+++ api/app.py
@@ -0,0 +1,135 @@
+# <!-- base line code : https://github.com/ibrahimokdadov/upload_file_python -->
+
+import os
+import PIL
+from PIL import Image
+from uuid import uuid4
+
+from flask import Flask, request, render_template, send_from_directory, send_file
+import paramiko
+from paramiko import SSHClient
+import time
+
+__author__ = 'ibininja'
+
+app = Flask(__name__, template_folder='../src')
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+file_name = None
+
+# Main page
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/upload")
+def index1():
+    return render_template("upload.html")
+
+@app.route("/intro")
+def index2():
+    return render_template("project_info.html")
+
+@app.route("/member")
+def index3():
+    return render_template("member.html")
+
+@app.route("/gallery")
+def index4():
+    return render_template("gallery.html")
+
+# Save uploaded image from user in public folder
+@app.route("/upload/uploader", methods=["POST"])
+def upload():
+    target = os.path.join(APP_ROOT, 'public/')
+  
+    print(target)
+    if not os.path.isdir(target):
+            os.mkdir(target)
+    else:
+        print("Couldn't create upload directory: {}".format(target))
+    print(request.files.getlist("file"))
+
+    for upload in request.files.getlist("file"):
+        print(upload)
+        print("{} is the file name".format(upload.filename))
+        filename = upload.filename
+        destination = "/".join([target, filename])
+        print ("Accept incoming file:", filename)
+        print ("Save it to:", destination)
+        upload.save(destination)
+
+    # Selected option from user 
+    style_option = request.form.get('style') 
+    emotion_option = request.form.get('emotion')
+    type_option = request.form.get('type')
+    model_option = request.form.get('model')
+    frame_option = request.form.get('frame')
+
+    # Convert image format to jpg & resize image
+    new_filename = Image.open(destination)
+    new_filename.save(destination.split('.')[0]+'.jpg') 
+    width, height = new_filename.size
+    if (width != 1024) or (height != 1204):        
+        img_resize_lanczos = new_filename.resize((1024, 1024), PIL.Image.LANCZOS)
+        img_resize_lanczos.save(destination.split('.')[0]+'.jpg') 
+    new_filename.close()
+
+    want = filename.split('.')[0]    
+    global file_name
+    file_name = want + "."+ type_option
+
+    localpath1=f'../src/public/{want}.png' 
+    remotepath1=f'/..../{want}.png' # server path
+    
+    # Connect to sever
+    client = SSHClient()
+    client.set_missing_host_key_policy(paramiko.client.AutoAddPolicy)
+    client.connect('....', username='....', password='....',port= ....)
+    sftp = client.open_sftp()
+    sftp.put(localpath1, remotepath1)
+    
+    
+    channel = client.invoke_shell()
+    channel.send('cd ....\n')
+    time.sleep(20)
+    channel.send('conda activate ....\n')
+    time.sleep(20)
+    channel.send(f'python emoticon_generate.py --file {want}.jpg --transform {style_option} --model {model_option} --duration {frame_option}\n')
+    time.sleep(20)
+    router_output = channel.recv(1024)
+ 
+
+    localpath2=f'../src/public/{want}.{type_option}'
+    remotepath2=f'/..../{want}.{type_option}'
+
+    sftp.get(remotepath2, localpath2)
+
+    sftp.close()
+    client.close()
+
+    return render_template("complete.html")
+
+
+@app.route('/upload/uploader/download')
+def down_page():
+    files = os.listdir("../src/public")
+    global file_name
+    op = file_name
+    return render_template('download.html', files=files, op=op)
+
+
+@app.route('/upload/uploader/filedown', methods = ['GET','POST'])
+def down_file():
+    if request.method == 'POST':
+        sw = 0
+        files = os.listdir("../src/public/")
+        for x in files:
+            if(x==request.form['file']):
+                sw=1 
+        
+        path = "public/"
+        return send_file(path + request.form['file'], attachment_filename = request.form['file'], as_attachment = True)
+    
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000, debug=True)
